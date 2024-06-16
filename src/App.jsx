7e8f367f070601c8b0bc2e23bcc00220a1688834
jess-ar <jessica18_24@hotmail.com>
2024-06-16 22:56:54 +0200

    Removed routing for pages and kept Navbar functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import Earbuds from './pages/Earbuds';
-import Watches from './pages/Watches';
-import Cart from './pages/Cart';
 
 const App = () => {
   return (
@@ -12,18 +8,10 @@ const App = () => {
         <Navbar />
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<Earbuds />} />
-          <Route path="/earbuds" element={<Earbuds />} />
-          <Route path="/watches" element={<Watches />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+      
       </main>
     </Router>
   );
 }
 
 export default App;
-
-
-
